574749781215075ae014704dd72db9f078e8a98e
Akikaze235 <AkikazeNoQwaserQwaser@gmail.com>
2023-02-28 09:16:00 +0700

    template dasar

---
+++ database/migrations/2023_02_28_020428_galery.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('galeri', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_kategori');
+            $table->string('judul_galeri');
+            $table->string('deskripsi_galeri');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('galeri');
+    }
+};
