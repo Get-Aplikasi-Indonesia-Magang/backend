88924f5fd4edb4c240a031a7eb822a851fd22c84
Akikaze235 <AkikazeNoQwaserQwaser@gmail.com>
2023-03-01 10:32:00 +0700

    update from galih

--- app/Http/Controllers/micrositeController.php
+++ app/Http/Controllers/micrositeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class micrositeController extends Controller
@@ -11,7 +12,11 @@ class micrositeController extends Controller
      */
     public function index()
     {
-        //
+        $user = User::all();
+        return response()->json([
+            'message' => 'success',
+            'data' => $user,
+        ], 200);
     }
 
     /**
@@ -19,7 +24,25 @@ class micrositeController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = $request->password;
+        $user->icon = $request->icon;
+        $user->id_role = $request->id_role;
+        $user->title = $request->title;
+        $user->bio = $request->bio;
+        $user->instagram = $request->instagram;
+        $user->twitter = $request->twitter;
+        $user->facebook = $request->facebook;
+        $user->youtube = $request->youtube;
+        $user->tiktok = $request->tiktok;
+
+        $user->save();
+        return response()->json([
+            'message' => 'success',
+            'data' => $user,
+        ], 200);
     }
 
     /**
@@ -27,7 +50,31 @@ class micrositeController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $user = User::find($id);
+        if (!$user) {
+            return response()->json([
+                'message' => 'not found',
+            ], 404);
+        }
+
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = $request->password;
+        $user->icon = $request->icon;
+        $user->id_role = $request->id_role;
+        $user->title = $request->title;
+        $user->bio = $request->bio;
+        $user->instagram = $request->instagram;
+        $user->twitter = $request->twitter;
+        $user->facebook = $request->facebook;
+        $user->youtube = $request->youtube;
+        $user->tiktok = $request->tiktok;
+
+        $user->update();
+        return response()->json([
+            'message' => 'success',
+            'data' => $user,
+        ], 200);
     }
 
     /**
@@ -35,6 +82,15 @@ class micrositeController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $user = User::find($id);
+        if (!$user) {
+            return response()->json([
+                'message' => 'not found',
+            ], 404);
+        }
+        $user->delete();
+        return response()->json([
+            'message' => 'success',
+        ], 200);
     }
 }
